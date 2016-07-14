614e20d3b17c9bb4e257c471602447d04ac21901
Francis <tfrancis@TFrancis-imac27.local>
2016-07-14 10:23:27 -0400

    Website

---
+++ script.js
@@ -0,0 +1,12 @@
+var dropdown = document.querySelector('.dropbtn')
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+dropdown.addEventListener('click', myFunction);
+
+    function ClickMe() {
+    window.open("https://vt.tumblr.com/tumblr_nt28m1hPTP1sk3f5o_480.mp4"); 
+}
